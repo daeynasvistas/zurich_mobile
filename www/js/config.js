f7dec75e76ca550b51bcf54330953c2bb2d4f40c
daeynasvistas <netrapida@live.com.pt>
2016-02-06 18:11:46 +0000

    Update and rename config.js-example to config.js

--- www/js/config.js
+++ www/js/config.js
@@ -1,4 +1,4 @@
 var CONFIG = {
-    FMS_URL: 'http://zurich.fixmystreet.staging.mysociety.org/',
+    FMS_URL: 'http://distrito.75.102.34.148.xip.io/',
     MAPIT_URL: 'http://global.mapit.mysociety.org/'
 };
